5925e9add5e8a1977576e9900d53d1d03eb14bf8
yuu3 <yuu3@users.noreply.github.com>
2015-06-03 10:27:35 +0900

    add: add files

---
+++ zsh/behind-window-notify/behind-window-notify.zsh
@@ -0,0 +1,24 @@
+
+autoload -Uz add-zsh-hook
+
+function remenber-cmd(){
+    export LASTCMD=$1
+}
+
+add-zsh-hook preexec remenber-cmd
+
+function behind-window-notify(){
+  if [[ -n $TMUX ]] then
+    current_panes=(${(@f)"$(tmux list-panes -F'#{pane_id}')"})
+
+    if [[ -z ${(M)current_panes#$TMUX_PANE} ]] then
+      if [[ ${OSTYPE} == darwin* ]] then
+        growlnotify -m "$LASTCMD" "コマンド終了: "
+      else
+	notify-send -t 3000 -u low "コマンド終了: " "$LASTCMD"
+       fi
+    fi
+  fi
+}
+
+add-zsh-hook precmd behind-window-notify
