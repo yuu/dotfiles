e2107e6b60807033b6fef92ccbfaac009bc5ed5e
yuu <yuu@users.noreply.github.com>
2017-08-22 14:18:19 +0900

    emacs: Delete package

--- emacs.d/elpa/niflheim-theme-20150630.821/niflheim-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; niflheim-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "niflheim-theme" "niflheim-theme.el" (21923
-;;;;;;  21506 545485 819000))
-;;; Generated autoloads from niflheim-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("niflheim-theme-pkg.el") (21923 21506
-;;;;;;  557527 730000))
-
-;;;***
-
-(provide 'niflheim-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; niflheim-theme-autoloads.el ends here
--- emacs.d/elpa/niflheim-theme-20150630.821/niflheim-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "niflheim-theme" "20150630.821" "A port of the Nifleim theme to Emacs" (quote nil))
