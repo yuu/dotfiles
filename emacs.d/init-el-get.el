c57a856eeed9c02433c22f148d310b7074550cc8
yuu <yuu@users.noreply.github.com>
2017-11-17 17:30:26 +0900

    emacs: change package manager

---
+++ emacs.d/init-el-get.el
@@ -0,0 +1,12 @@
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get-bundle init-loader)
+(el-get-bundle helm)
+(el-get-bundle magit :branch "2.6.2")
+(el-get-bundle elpa:hc-zenburn-theme)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,12 +1,14 @@
-;; package
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+(load (concat user-emacs-directory "init-el-get.el"))
+
+(load-theme 'hc-zenburn t)
 
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
-(init-loader-load "~/.emacs.d/inits")
+(init-loader-load (concat user-emacs-directory "inits"))
 
 ;; ~/.emacs.d/site-lisp 以下全部読み込み
 (let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
