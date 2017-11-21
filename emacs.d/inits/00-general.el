f096efec9c19036c559dbc3bef11048ac6b50453
yuu <yuu@users.noreply.github.com>
2017-11-21 09:42:32 +0900

    emacs: remove comment

--- emacs.d/inits/00-general.el
+++ emacs.d/inits/00-general.el
@@ -51,22 +51,3 @@
       scroll-margin 0
       scroll-step 1)
 (setq comint-scroll-show-maximum-output t) ;; shell-mode
-
-;; モードラインの割合表示を総行数表示
-;; (defvar my-lines-page-mode t)
-;; (defvar my-mode-line-format)
-
-;; (when my-lines-page-mode
-;;   (setq my-mode-line-format "%d")
-;;   (if size-indication-mode
-;;       (setq my-mode-line-format (concat my-mode-line-format " of %%I")))
-;;   (cond ((and (eq line-number-mode t) (eq column-number-mode t))
-;;          (setq my-mode-line-format (concat my-mode-line-format " (%%l,%%c)")))
-;;         ((eq line-number-mode t)
-;;          (setq my-mode-line-format (concat my-mode-line-format " L%%l")))
-;;         ((eq column-number-mode t)
-;;          (setq my-mode-line-format (concat my-mode-line-format " C%%c"))))
-
-;;   (setq mode-line-position
-;;         '(:eval (format my-mode-line-format
-;;                         (count-lines (point-max) (point-min))))))
