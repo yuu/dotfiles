594ea912f7d7a9441823a4b7633e80f745a6fb76
yuu <yuu@users.noreply.github.com>
2017-11-22 11:07:54 +0900

    emacs: update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,12 +8,6 @@
 (setq init-loader-show-log-after-init nil)
 (init-loader-load (concat user-emacs-directory "inits"))
 
-;; ~/.emacs.d/site-lisp 以下全部読み込み
-(let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
-  (add-to-list 'load-path default-directory)
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-  (normal-top-level-add-subdirs-to-load-path)))
-
 (setq-default message-log-max nil)
 (kill-buffer "*Messages*")
 (kill-buffer "*scratch*")
