3930202e5e83e84346ed9781b27de18c17f3c647
yuu <yuu@users.noreply.github.com>
2017-12-31 14:15:12 +0900

    emacs: configure org-mode

--- emacs.d/inits/20-how-org.el
+++ emacs.d/inits/20-how-org.el
@@ -12,10 +12,16 @@
 
 
 ;; org-mode
-(global-set-key (kbd "C-x c") 'org-capture)
+;; These lines only if org-mode is not part of the X/Emacs distribution.
+(autoload 'org-mode "org" "Org mode" t)
+(autoload 'org-diary "org" "Diary entries from Org mode")
+(autoload 'org-agenda "org" "Multi-file agenda from Org mode" t)
 
-(add-hook 'org-mode-hook 'howm-mode)
+(global-set-key (kbd "C-x cc") 'org-capture)
+(global-set-key (kbd "C-x ca") 'org-agenda)
+
 (add-to-list 'auto-mode-alist '("\\.txt$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
 (setq org-startup-with-inline-image t)
 ;; DONEの時刻を記録
@@ -24,12 +30,19 @@
 (setq org-directory "~/Dropbox/org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 (setq org-mobile-inbox-for-pull (concat org-mobile-directory "/notes.org"))
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-(require 'find-lisp)
-(setq org-agenda-files
-    (find-lisp-find-files org-directory "\.org$"))
 
-(setq org-agenda-files
-      (mapcar (lambda (basename)
-                (concat org-directory (symbol-name basename) ".org"))
-              '(f3 f1 f2)))
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+(setq org-agenda-files (file-expand-wildcards (concat org-directory "/*.org")))
+
+; http://orgmode.org/manual/Template-elements.html#Template-elements
+; http://orgmode.org/manual/Template-expansion.html#Template-expansion
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/Dropbox/org/task.org" "Tasks")
+             "* TODO %?\n  %U")
+        ("n" "Note" entry (file+headline "~/Dropbox/org/notes.org" "Notes")
+             "* %?")
+        ("j" "Journal" entry (file+datetree "~/Dropbox/org/journal.org")
+             "* %?")
+        ("r" "Reading" entry (file+function "~/Dropbox/org/reading.org" helm-occur)
+             "* %?")
+         ))
