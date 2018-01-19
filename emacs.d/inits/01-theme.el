672d87c290921a67efdace8558008454d9acb6db
yuu <yuu@users.noreply.github.com>
2018-01-19 22:34:00 +0900

    emacs: Fix color, Enable transpancy for gui emacs

--- emacs.d/inits/01-theme.el
+++ emacs.d/inits/01-theme.el
@@ -1,2 +1,7 @@
 ;(load-theme 'niflheim t)
 (load-theme 'hc-zenburn t)
+  (set-frame-parameter nil 'alpha 80) ;transpaent on gui emacs
+
+  (custom-set-faces
+   '(company-preview ((t (:background "green"))))
+  )
