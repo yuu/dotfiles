e8936da28fa676d94ec18ec3f57e21d774fb60d8
yuu <yuu@users.noreply.github.com>
2018-01-22 11:24:43 +0900

    emacs: backword kill line

--- emacs.d/inits/99-keybind.el
+++ emacs.d/inits/99-keybind.el
@@ -8,3 +8,8 @@
       (kill-region (point) (mark))
     (backward-kill-word 1)))
 (global-set-key (kbd "C-w") 'kill-region-or-backward-kill-word)
+
+;; backward kill line
+(global-set-key (kbd "M-k")(lambda ()
+                            (interactive)
+                            (kill-line 0)))
