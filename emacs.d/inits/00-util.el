a419c29df85ace5ffb88ec015d6b0820fbf1f36b
yuu <yuu@users.noreply.github.com>
2018-02-27 14:55:31 +0900

    emacs: interactive change indent-tabs-mode

--- emacs.d/inits/00-util.el
+++ emacs.d/inits/00-util.el
@@ -1,2 +1,14 @@
 (require 'undo-tree)
 (global-undo-tree-mode t)
+
+(defun enable-tabs-mode ()
+  "Enable indent-tabs-mode"
+  (interactive)
+  (setq indent-tabs-mode t))
+(provide 'enable-tabs-mode)
+
+(defun disable-tabs-mode ()
+  "Disable indent-tabs-mode"
+  (interactive)
+  (setq indent-tabs-mode nil))
+(provide 'disable-tabs-mode)
