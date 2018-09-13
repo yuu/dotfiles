6ee36c93a35d39a08caea75a0f4aec867dce20d8
yuu <yuu@users.noreply.github.com>
2018-09-13 16:43:32 +0900

    emacs: Add python3 quickrun

--- emacs.d/inits/20-python.el
+++ emacs.d/inits/20-python.el
@@ -2,3 +2,8 @@
   (setq jedi:complete-on-dot t)
   (setq jedi:use-shortcuts t))
 (add-to-list 'company-backends 'company-jedi)
+
+(with-eval-after-load 'quickrun
+  (quickrun-add-command "python3"
+    '((:command . "python3"))
+    :default "python"))
