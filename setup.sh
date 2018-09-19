7379a5dcea8b9d240f1332415a4e01c40b1b8b0a
yuu <yuu@users.noreply.github.com>
2018-09-19 09:30:10 +0900

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -9,32 +9,38 @@ function echo_blue() { echo -e "\e[2;34m$*\e[m"; }
 #----------------------------------------------------------------------------#
 # dotfiles                                                                   #
 #----------------------------------------------------------------------------#
-echo_blue link: dotfiles
 # git
+echo_blue link: git
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/git/gitignore ~/.gitignore
 cp    ~/dotfiles/git/gitconfig.local ~/.gitconfig.local
 
 # tmux
+echo_blue link: tmux
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 git clone git://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # zsh
+echo_blue link: zsh
 mkdir -p ~/.config/zsh
 ln -s $(pwd)/zsh/* ~/.config/zsh/
 ln -s ~/.config/zsh/zshrc ~/.zshrc
 git clone git://github.com/zsh-users/zaw.git ~/.config/zsh/zaw
 git clone git://github.com/felixr/docker-zsh-completion.git ~/.config/zsh/docker
 
-# vim
-VIM="$HOME/.config/nvim"
-mkdir -p $VIM
-for i in $(command ls nvim |grep -v local); do echo $i; ln -s $(pwd)/nvim/$i $VIM/; done
+# emacs
+echo_blue link: emacs
+EMCONF="$HOME/.emacs.d"
+mkdir -p $EMCONF
+for i in $(command ls emacs); do echo $i; ln -s $(pwd)/emacs/$i $EMCONF/; done
 
 # peco
+echo_blue link: peco
 ln -s $(pwd)/peco ~/.peco
 
 # other
+ln -s clang-format ~/.clang-format
+ln -s rtorrent.rc ~/.rtorrent.rc
 mkdir ~/.config/python
 ln -s $(pwd)/pythonstartup ~/.config/python/pythonstartup
 ln -s $(pwd)/screenrc ~/.screenrc
@@ -45,7 +51,8 @@ if [ "$(uname)" == 'Darwin' ]; then
     ln -s ~/dotfiles/karabiner.json $KD
     ln -s ~/dotfiles/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 else
-    mkdir -p ~/.config/terminator
-    ln -s ~/dotfiles/terminator/config ~/.config/terminator/
+    TC=~/.config/terminator
+    mkdir -p $TC
+    ln -s ~/dotfiles/terminator/config $TC/
 fi
 echo_success success
