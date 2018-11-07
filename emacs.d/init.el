0c1356418e83ee1385e9b654f8fc7e44e815dcf8
yuu <yuuxzy@gmail.com>
2018-11-07 10:41:46 +0900

    emacs: Fix unnecessary

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,10 +1,7 @@
-;;(package-initialize)
-
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 (load (concat user-emacs-directory "init-el-get.el"))
 
-(require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load (concat user-emacs-directory "inits"))
 (custom-set-variables
