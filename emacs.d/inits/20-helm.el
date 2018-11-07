b87606e2f8bdb467468144afa68755793954d8f1
yuu <yuuxzy@gmail.com>
2018-11-07 10:55:37 +0900

    emacs: use helm-mini

--- emacs.d/inits/20-helm.el
+++ emacs.d/inits/20-helm.el
@@ -2,13 +2,11 @@
   (helm-mode 1)
 
   (define-key global-map (kbd "M-x")     'helm-M-x)
-  (define-key global-map (kbd "C-x C-r") 'helm-recentf)
   (define-key global-map (kbd "M-y")     'helm-show-kill-ring)
   (define-key global-map (kbd "C-c i")   'helm-imenu)
   (define-key global-map (kbd "C-M-z")   'helm-resume)
   (define-key global-map (kbd "C-x C-f")  'helm-find-files)
-  (define-key global-map (kbd "C-x b")   'helm-buffers-list)
-  (define-key global-map (kbd "M-g g")   'helm-do-grep)
+  (define-key global-map (kbd "C-x b")   'helm-mini)
   (define-key global-map (kbd "M-g l")   'helm-occur-from-isearch)
 
   (define-key global-map [remap find-file] 'helm-find-files)
@@ -17,6 +15,12 @@
   (define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
 
   (define-key helm-map (kbd "C-h") 'delete-backward-char)
+
+(setq helm-mini-default-sources
+      '(helm-source-buffers-list
+        helm-source-bookmarks
+        helm-source-recentf
+        helm-source-file-cache))
 
 (global-unset-key (kbd "C-x c c")) ; helm-colors
 (global-unset-key (kbd "C-x c a")) ; helm-apropos
