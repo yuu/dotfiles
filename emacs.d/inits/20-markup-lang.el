d340020b2ea448aff1c8379c036e82c1d8366388
yuu <yuu@users.noreply.github.com>
2019-02-15 18:11:26 +0900

    emacs: configuration markdown mode

--- emacs.d/inits/20-markup-lang.el
+++ emacs.d/inits/20-markup-lang.el
@@ -1,5 +1,13 @@
 (autoload 'gfm-mode "markdown-mode" "Major mode for editing GitHub Flavored Markdown files" t)
+(with-eval-after-load 'markdown-mode
+    (setq
+        markdown-split-window-direction 'right
+        markdown-command "github-markup"
+        markdown-command-needs-filename t
+        markdown-css-paths (list "https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css")))
+
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+
 (autoload 'plantuml-mode "plantuml-mode" "plantuml-mode" t)
 (add-to-list 'auto-mode-alist '("\\.puml\\'" . plantuml-mode))
 (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
