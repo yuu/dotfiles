68d7c8a12f7165532da80ca507a0005129b94400
yuu <yuuxzy@gmail.com>
2019-06-25 13:32:30 +0900

    emacs: support wsl clipboard copy

--- emacs.d/inits/linux-custom.el
+++ emacs.d/inits/linux-custom.el
@@ -1,7 +1,18 @@
+(defun isWSL ()
+  "run emacs on windows subsystem linux ?"
+  (if (string= "" (shell-command-to-string "uname -r |grep Microsoft"))
+    nil
+    t))
+
 (defun clipboard-copy ()
   (interactive)
   (when (region-active-p)
-    (shell-command-on-region (region-beginning) (region-end) "xsel --clipboard --input" nil nil)))
+    (if (isWSL)
+      (shell-command-on-region (region-beginning) (region-end) "clip.exe" nil nil)
+      (shell-command-on-region (region-beginning) (region-end) "xsel --clipboard --input" nil nil)
+      )
+   )
+)
 
 (use-package rg
   :config
