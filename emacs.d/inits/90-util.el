50b86d55ccd79e7106f9936d489fab90e054095e
yuu <yuu@users.noreply.github.com>
2024-04-29 00:05:38 +0900

    eamcs: add window-resize for C-x w r to hjkl

--- emacs.d/inits/90-util.el
+++ emacs.d/inits/90-util.el
@@ -214,4 +214,40 @@
   "Convert camelCase STRING to CapitalCase."
   (mapconcat 'identity (mapcar 'capitalize (split-string-and-unquote string "[A-Z][^A-Z]*")) ""))
 
+(defun window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+         (current-width (window-width))
+         (current-height (window-height))
+         (dx (if (= (nth 0 (window-edges)) 0) 1
+               -1))
+         (dy (if (= (nth 1 (window-edges)) 0) 1
+               -1))
+         action c)
+    (catch 'end-flag
+      (while t
+        (setq action
+          (read-key-sequence-vector (format "size[%dx%d]"
+                                      (window-width)
+                                      (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?l)
+                (enlarge-window-horizontally dx))
+          ((= c ?h)
+            (shrink-window-horizontally dx))
+          ((= c ?j)
+            (enlarge-window dy))
+          ((= c ?k)
+            (shrink-window dy))
+          ;; otherwise
+          (t
+            (let ((last-command-char (aref action 0))
+                   (command (key-binding action)))
+              (when command
+                (call-interactively command)))
+            (message "Quit")
+            (throw 'end-flag t)))))))
+(global-set-key "\C-xwr" 'window-resizer)
+
 ;;; 90-util.el ends here
