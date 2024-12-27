832395307a582e7036629ddf8dc2665cb3687963
yuu <yuu@users.noreply.github.com>
2024-12-27 17:39:16 +0900

    emacs: fix yas conf

--- emacs.d/inits/11-package.el
+++ emacs.d/inits/11-package.el
@@ -138,9 +138,7 @@
     ([(tab)] . nil)
     ("TAB" . nil))
   :init
-  (add-hook 'after-init-hook 'yas-global-mode)
-  :config
-  (add-to-list 'warning-suppress-types '(yasnippet backquote-change)))
+  (add-hook 'after-init-hook 'yas-global-mode))
 
 ;;; vsc
 (defun magit-display-buffer-status-same-window (buffer)
