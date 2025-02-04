0a2a66c5c77289b7aec680b1e40e9edd27f816c8
yuu <yuu@users.noreply.github.com>
2025-02-04 22:45:17 +0900

    fix: color.sh

--- bin/color.sh
+++ bin/color.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for i in {255..0} ; do
-    printf "\x1b[38;5;${i}mcolour${i} "
+for i in {0..255}; do
+    printf "\033[48;5;%sm %3d \033[0m" "$i" "$i"
+    if [ $(( (i+1) % 16 )) -eq 0 ]; then
+        echo
+    fi
 done
-printf "\n"
