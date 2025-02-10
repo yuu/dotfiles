4a99f66c0cb3a168ab34cbedc58dc6da09e1ecda
yuu <yuu@users.noreply.github.com>
2025-02-10 23:53:28 +0900

    zsh: fix terminal notify

--- zsh/behind-window-notify/behind-window-notify.zsh
+++ zsh/behind-window-notify/behind-window-notify.zsh
@@ -7,22 +7,15 @@ function remenber-cmd(){
 
 add-zsh-hook preexec remenber-cmd
 
-# local terminal_identity="com.apple.Terminal"
-local terminal_identity="com.googlecode.iterm2"
+# $ osascript -e 'id of app "WezTerm"'
+local terminal_identity="com.github.wez.wezterm"
 
 function behind-window-notify() {
-  if [[ -n $TMUX ]] then
-    current_panes=(${(@f)"$(tmux list-panes -F'#{pane_id}')"})
-    if [[ -z ${(M)current_panes#$TMUX_PANE} ]] then
-
-      if [[ ${OSTYPE} == darwin* ]] then
-        ret=$?
-        terminal-notifier -title "$LASTCMD" -message "$ret" -activate $terminal_identity
-      else
-        notify-send -t 3000 -u low "コマンド終了: " "$LASTCMD"
-      fi
-
-    fi
+  if [[ ${OSTYPE} == darwin* ]] then
+    ret=$?
+    terminal-notifier -title "$LASTCMD" -message "$ret" -activate $terminal_identity
+  else
+    notify-send -t 3000 -u low "コマンド終了: " "$LASTCMD"
   fi
 }
 
