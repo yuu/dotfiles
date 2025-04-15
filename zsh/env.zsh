77c973de9b0e9d8d01346bf445cc80a14bc4c35e
yuu <yuu@users.noreply.github.com>
2025-04-15 15:42:28 +0900

    zsh: specify LC_

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,7 @@
 export TERM=xterm-256color
 export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_COLLATE=C
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 export GOPATH=$HOME/.local
 export GPG_TTY=`tty`
