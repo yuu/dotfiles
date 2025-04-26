c180d03ef02f1f96cd22de4e6f924aa88a998404
yuu <yuu@users.noreply.github.com>
2025-04-26 10:33:35 +0900

    fix(zsh): disable behind window notify

--- zsh/behind-window-notify/behind-window-notify.zsh
+++ zsh/behind-window-notify/behind-window-notify.zsh
@@ -1,7 +1,7 @@
 
 autoload -Uz add-zsh-hook
 
-function remenber-cmd(){
+function remenber-cmd() {
     export LASTCMD=$1
 }
 
@@ -10,13 +10,59 @@ add-zsh-hook preexec remenber-cmd
 # $ osascript -e 'id of app "WezTerm"'
 local terminal_identity="com.github.wez.wezterm"
 
-function behind-window-notify() {
-  if [[ ${OSTYPE} == darwin* ]] then
-    ret=$?
-    terminal-notifier -title "$LASTCMD" -message "$ret" -activate $terminal_identity
-  else
-    notify-send -t 3000 -u low "コマンド終了: " "$LASTCMD"
-  fi
+function on_wezterm() {
+    [[ -n $WEZTERM_PANE ]]
 }
 
-add-zsh-hook precmd behind-window-notify
+function on_tmux() {
+    [[ -n $TMUX ]]
+}
+
+function is_wezterm_focus() {
+    current_pane=$(wezterm cli list --format json |jq ".[] | select(.is_active==true) |.pane_id" |xargs)
+    if [[ -n ${current_pane[(re)$WEZTERM_PANE]} ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function is_tmux_focus() {
+    current_panes=(${(@f)"$(tmux list-panes -F'#{pane_id}')"})
+    [[ -z ${(M)current_panes#$TMUX_PANE} ]]
+}
+
+function notify() {
+    local title=$1
+    local message=$2
+    local option="-activate ${terminal_identity}"
+
+    if [[ ${OSTYPE} == darwin* ]]; then
+        terminal-notifier -title "${title}" -message "${message}" ${option}
+    else
+        notify-send -t 3000 -u low "${message}"
+    fi
+}
+
+function _behind-window-notify() {
+    if on_wezterm; then
+        if ! is_wezterm_focus; then
+            if [[ -n $LASTCMD ]]; then
+                notify "$LASTCMD" "exit"
+            fi
+        fi
+    fi
+}
+
+function _behind-window-notify-tmux() {
+    if on_tmux; then
+        current_panes=(${(@f)"$(tmux list-panes -F'#{pane_id}')"})
+        if is_tmux_focus; then
+            if [[ -n $LASTCMD ]]; then
+                notify "$LASTCMD" "exit"
+            fi
+        fi
+    fi
+}
+
+# add-zsh-hook precmd _behind-window-notify
