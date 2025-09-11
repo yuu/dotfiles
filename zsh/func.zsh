354239ed5bd5253aa242235d648bac93693ce152
yuu <yuu@users.noreply.github.com>
2025-09-11 22:48:44 +0900

    zsh: add fzf utility

--- zsh/func.zsh
+++ zsh/func.zsh
@@ -11,6 +11,33 @@ function ghq-list() {
 zle -N ghq-list
 bindkey '^xl' ghq-list
 
+function fzf-cdr() {
+    local selected_dir=$(cdr -l | awk '{ print $2 }' | fzf --reverse)
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N fzf-cdr
+bindkey "^[r" fzf-cdr
+
+function fzf-select-history() {
+    BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N fzf-select-history
+bindkey '^r' fzf-select-history
+
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    [[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
+    zle end-of-line                 # move cursor to end-of-line
+}
+zle -N sudo-command-line
+bindkey "\e\e" sudo-command-line    # [Esc] [Esc]
+
 function confirm() {
   echo -n "Run '$*'? [y/N] "
   read -q && echo "" && "$@" || echo "Canceled"
