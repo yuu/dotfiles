8539cf6d400f03cbd0a01e73ef2fc2ffed51729b
yuu <yuu@users.noreply.github.com>
2025-09-11 22:37:23 +0900

    zsh: update conf

--- zsh/func.zsh
+++ zsh/func.zsh
@@ -11,53 +11,6 @@ function ghq-list() {
 zle -N ghq-list
 bindkey '^xl' ghq-list
 
-fd-peco-files() {
-    selection=$(fd --type f --type d . | peco)
-
-    if [[ -n "$selection" ]]; then
-        LBUFFER+="${(q)selection}"
-        zle redisplay
-    fi
-}
-
-zle -N fd-peco-files
-bindkey '^o' fd-peco-files
-
-_navi_call() {
-   local result="$(navi "$@" </dev/tty)"
-   printf "%s" "$result"
-}
-
-_navi_widget() {
-   local -r input="${LBUFFER}"
-   local -r last_command="$(echo "${input}" | navi fn widget::last_command)"
-   local replacement="$last_command"
-
-   if [ -z "$last_command" ]; then
-      replacement="$(_navi_call --print)"
-   elif [ "$LASTWIDGET" = "_navi_widget" ] && [ "$input" = "$previous_output" ]; then
-      replacement="$(_navi_call --print --query "$last_command")"
-   else
-      replacement="$(_navi_call --print --best-match --query "$last_command")"
-   fi
-
-   if [ -n "$replacement" ]; then
-      local -r find="${last_command}_NAVIEND"
-      previous_output="${input}_NAVIEND"
-      previous_output="${previous_output//$find/$replacement}"
-   else
-      previous_output="$input"
-   fi
-
-   zle kill-whole-line
-   LBUFFER="${previous_output}"
-   region_highlight=("P0 100 bold")
-   zle redisplay
-}
-
-zle -N _navi_widget
-bindkey '^xj' _navi_widget
-
 function confirm() {
   echo -n "Run '$*'? [y/N] "
   read -q && echo "" && "$@" || echo "Canceled"
--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -8,3 +8,20 @@ alias lla="ls -laFGh"
 alias date="date +%Y-%m-%dT%H:%M:%S%z"
 alias today="command date '+%Y-%m-%d'"
 alias now="command date '+%H:%M:%S'"
+
+alias z=yazi
+
+path=(/opt/homebrew/bin(N-/) /opt/homebrew/sbin(N-/) ${path})
+fpath=(/opt/homebrew/share/zsh-completions(N-/) /opt/homebrew/share/zsh/site-functions(N-/) ${fpath})
+
+source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+source <(zoxide init zsh)
+source <(fzf --zsh)
+
+source <(navi widget zsh)
+bindkey -r '^G'
+bindkey "^[j" _navi_widget
+
+source <(mise activate zsh)
+. "$HOME/.cargo/env"
